e02620919a80506a88f1d9c5006fa808acf2b2e9
Ke Xu <morefree7@gmail.com>
2014-08-06 00:47:10 -0700

    initial commit

--- src/test/scala/raft/TestLeaderElection.scala
+++ src/test/scala/raft/TestLeaderElection.scala
@@ -4,11 +4,11 @@ import org.junit._
 import java.util.concurrent._
 
 class TestLeaderElection {
-	@Test
-	def testLeader = {
-	  println("Hello, World")
-	  Node.start
-	  TimeUnit.SECONDS.sleep(15)
-	  Node.stop
-	}
+  @Test
+  def testLeader = {
+    println("Hello, World")
+    Node.start
+    TimeUnit.SECONDS.sleep(15)
+    Node.stop
+  }
 }
